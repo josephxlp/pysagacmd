ea21f826983c204c8651580712471d6b345faff6
jzphlp <josephluntadilap@gmail.com>
2025-03-01 19:12:28 +0000

    uvars added

---
+++ uvars.py
@@ -0,0 +1,43 @@
+ot_dpath = r"C:\Users\Joseph\Documents\UoE\Morpho\data\OPEN_TOPOGRAPHY"
+proccesed_dpath = r"data\processed"
+raw_dpath = r"data\raw"
+
+tilenames = [
+ 'N09E105',
+ 'N09E106',
+ 'N10E104',
+ 'N10E105',
+ 'N10E106',
+ 'N11E104',
+ 'N11E105',
+ 'N12E103',
+ 'N12E104',
+ 'N12E105',
+ 'N13E103',
+ 'N13E104',
+ 'N13E105',
+ 'S01W063',
+ 'S01W064',
+ 'S02W063',
+ 'S02W064']
+
+varnames =[
+ 'COP90',
+ 'AW3D30',
+ 'AW3D30_E',
+ 'COP30',
+ 'EU_DTM',
+ 'GEBCOIceTopo',
+ 'GEBCOSubIceTopo',
+ 'GEDI_L3',
+ 'NASADEM',
+ 'SRTM15Plus',
+ 'SRTMGL1',
+ 'SRTMGL1_E']
+
+sagapath = r"c:\Users\Joseph\Downloads\saga-9.7.2_x64\saga-9.7.2_x64\saga_cmd.exe"
+saga_cmd_path = r"c:\Users\Joseph\Downloads\saga-9.7.2_x64\saga-9.7.2_x64\saga_cmd.exe"
+saga_gui_path = r"c:\Users\Joseph\Downloads\saga-9.7.2_x64\saga-9.7.2_x64\saga_gui.exe"
+
+gpkg_pattern = r"C:\Users\Joseph\Documents\DATASETX\vgrid\vector_tiles\gpkg_tiles\*.gpkg"
+
